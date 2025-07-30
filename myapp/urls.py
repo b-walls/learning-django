cc2067e292f35883e19d772c39bcb6d9980664b2
b-walls <brendan.walls22@gmail.com>
2025-07-30 14:05:13 -0400

    Various templating practice

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('hello/', views.hello),
-    path('menu/<int:category>', views.menu),
-    path('attendance/', views.attendance_view)
+    path('menu/<int:category>', views.menu_by_category),
+    path('menu/', views.menu),
+    path('attendance/', views.attendance_view),
+    path('about/', views.about),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from myapp.forms import AttendanceForm
 from .models import Menu
 
@@ -12,7 +12,7 @@ def home(request):
 def hello(request):
     return HttpResponse("Hello World!")
 
-def menu(request, category):
+def menu_by_category(request, category):
     items = Menu.objects.filter(category_id=category)
     html = f"<h1>{category}(s)</h1>"
 
@@ -20,6 +20,12 @@ def menu(request, category):
         html += f"<h2>{item.menu_item} : ${item.price}</h2>"
 
     return HttpResponse(html)
+
+def menu(request):
+    items = Menu.objects.all()
+    context = {'menu': items}
+    return render(request, 'menu.html', context=context)
+    
 
 def attendance_view(request):
     form = AttendanceForm()
@@ -29,3 +35,7 @@ def attendance_view(request):
             form.save()
     context = {"form": form}
     return render(request, "home.html", context)
+
+def about(request):
+    about_content = {'about': "Based in Chicago, Illinois, Little Lemon is an amazing restaurant!!"}
+    return render(request, "about.html", context=about_content)
